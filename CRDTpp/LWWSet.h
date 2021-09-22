217ecb55ab9ac4c327d63e73521785922c88bba2
ckyy1g20 <ckyy1g20@soton.ac.uk>
2021-09-22 22:04:34 +0100

    Optimised LWW-Element-Set

--- CRDTpp/LWWSet.h
+++ CRDTpp/LWWSet.h
@@ -3,39 +3,49 @@
 #include <set>
 #include <unordered_set>
 #include <algorithm>
+#include <unordered_map>
 
 template<typename T>
 class LWWSet {
 public:
 	std::unordered_set<T> value() const {
 		std::unordered_set<T> result;
-		for(const auto& added_item : add_set) {
-			bool should_add = true;
-			for(const auto& removed_item : remove_set) {
-				if(removed_item.first == added_item.first && removed_item.second > added_item.second) {
-					should_add = false;
-					break;
+		for(auto iter = add_set.cbegin(); iter != add_set.cend(); iter++) {
+			if(remove_set.count(iter->first)) {
+				if(iter->second >= remove_set.at(iter->first)) {
+					result.insert(iter->first);
 				}
 			}
-			if(should_add) {
-				result.insert(added_item.first);
+			else {
+				result.insert(iter->first);
 			}
 		}
 		return result;
 	};
 	void add(T elem, int timestamp) {
-		add_set.insert({elem, timestamp});
+		if(timestamp > add_set[elem]) {
+			add_set[elem] = timestamp;
+		}
 	};
 	void remove(T elem, int timestamp) {
-		remove_set.insert({elem, timestamp});
+		if(timestamp > remove_set[elem]) {
+			remove_set[elem] = timestamp;
+		}
 	};
 	void merge(const LWWSet& other) {
-		add_set.insert(other.add_set.begin(), other.add_set.end());
-		remove_set.insert(other.remove_set.begin(), other.remove_set.end());
+		for(auto it = other.add_set.cbegin(); it != other.add_set.cend(); it++) {
+			if(it->second > add_set[it->first]) {
+				add_set[it->first] = it->second;
+			}
+		}
+		for(auto itr = other.remove_set.cbegin(); itr != other.remove_set.cend(); itr++) {
+			if(itr->second > remove_set[itr->first]) {
+				remove_set[itr->first] = itr->second;
+			}
+		}
 	};
 
 private:
-	// Could optimise by replacing these sets with hash tables so for faster lookup of the timestamps of an object
-	std::set<std::pair<T, int>> add_set;
-	std::set<std::pair<T, int>> remove_set;
+	std::unordered_map<T, int> add_set;
+	std::unordered_map<T, int> remove_set;
 };
